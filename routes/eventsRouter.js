fc1da5f6d68293bd8bf732072a380a1a8fb34f02
olliv <olliav9@gmail.com>
2018-11-21 18:23:53 +0200

    Creating CRUD-functionality

--- routes/eventsRouter.js
+++ routes/eventsRouter.js
@@ -4,10 +4,71 @@ var router = express.Router();
 var Event = require("../models/Event");
 
 router.get("/", (req, res) => {
-	res.render("../views/index");
+
+	Event.find({}, (err, data) => {
+
+		if(err) {
+			console.log(err);
+			res.send(err);
+		}
+
+		res.render("../views/index", {
+			events:data
+		});
+	});
 });
 
-router.post("/", (req,res) => {
+router.get("/delete/:id", (req,res) => {
+
+	var id = req.params.id;
+	
+	Event.findByIdAndRemove(id, (err) => {
+		if(err) {
+			console.log(err);
+			res.send(err);
+		}
+		res.redirect("/events");
+	})
+});
+
+
+// Form for edit
+router.get("/edit/:id", (req,res) => {
+
+	var id = req.params.id;
+
+	Event.findById(id, (err, event) => {
+		if(err) {
+			console.log(err);
+			res.send(err);
+		}
+
+		res.render("../views/edit", {
+			data:event
+		});
+	});
+});
+
+router.post("/edit/:id", (req, res) => {
+
+	var id = req.params.id;
+
+
+	Event.findOneAndUpdate({_id:id}, req.body, function(err, result) {
+
+        if(err){
+            console.log(err);
+        }
+
+        console.log("RESULT: " + result);
+
+        res.send('Done');
+    });
+
+});
+
+
+router.post("/create", (req,res) => {
 
 	var name = req.body.name;
 	var desc = req.body.description;
